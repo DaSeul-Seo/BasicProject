a1a98407d209173660a237fdfcb97c9d1e0a559f
서다슬 <star.dss999@gmail.com>
2026-06-12 12:10:53 +0900

    Add files via upload

---
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Atfernoon";
+            return View();
+            //return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponce guestResponce)
+        {
+            if (ModelState.IsValid)
+            {
+                // 파티 주최자에게 전자 메일로 응답한다.
+                return View("Thanks", guestResponce);
+            }else
+            {
+                // 유효성 오류 존재
+                return View();
+            }
+        }
+
+        // GET: Home
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public string Index()
+        //{
+        //    return "Hello World";
+        //}
+    }
+}
---
+++ PartyInvites/PartyInvites/Models/GuestResponce.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponce
+    {
+        [Required(ErrorMessage = "Please enter you name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
+        // true, false, null 값을 가질 수 있는 Nullable bool 형태 => 유효성 검사
+        public bool? WillAttend { get; set; }
+        
+    }
+}
